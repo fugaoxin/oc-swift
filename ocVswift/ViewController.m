c4457ff77d4ec73211e7c704527742bf9aef2d10
123 <123@SINO-2015-0192-GZ-GaoXin-Fu.local>
2016-08-08 13:52:14 +0800

    new project

---
+++ ocVswift/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ocVswift
+//
+//  Created by 123 on 16/7/28.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ocVswift-Swift.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    swiftTest * ww=[[swiftTest alloc] init];
+    [ww test];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ocVswift/swiftTest.swift
@@ -0,0 +1,39 @@
+//
+//  swiftTest.swift
+//  ocVswift
+//
+//  Created by 123 on 16/7/28.
+//  Copyright © 2016年 123. All rights reserved.
+//
+
+import UIKit
+
+class swiftTest: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func test(){
+        print("hello!! welcome to swift")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
